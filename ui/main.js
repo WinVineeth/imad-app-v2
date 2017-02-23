e6654bbe38f205f29e29d1d9861ba6c85960f5c3
WinVineeth <winningvineeth@gmail.com>
2017-02-23 16:35:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,11 @@
 console.log('Loaded!');
 
 var im=document.getElementById('winvin');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    im.style.marginLeft=marginLeft+'px';
+}
 im.onclick=function(){
-  im.style.marginLeft='100px';  
+    var interval=setInterval(moveRight,100);
 };
