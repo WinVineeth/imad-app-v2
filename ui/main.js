febaad4010f16a728943f44d2471c99d447322ec
WinVineeth <winningvineeth@gmail.com>
2017-02-24 06:22:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
-
+var counter=0;
 var im=document.getElementById('winvin');
 var marginLeft=0;
 function moveRight(){
@@ -8,4 +8,10 @@ function moveRight(){
 }
 im.onclick=function(){
     var interval=setInterval(moveRight,50);
+};
+var t1=document.getElementById('count');
+t1.onclick=function(){
+  counter=counter+1;
+  var t2=document.getElementById('cou');
+  t2.innerHTML=counter.toString();
 };
